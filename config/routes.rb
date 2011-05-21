ed9ead27fb2046672709b96bd4c87ec080228d66
Ben Gross <brgross@gmail.com>
2011-05-21 16:06:31 -0400

    pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Dayrobot::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "home/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
