cc685ad42e320cc09a91e651ba1aa46cc6e23652
Ben Gross <brgross@gmail.com>
2011-05-21 15:53:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayrobot::Application.config.secret_token = '7ec67ed36acc37a2ee251fbbab858c17f4323f62e72a79b5dd3eb4507def060d88b202092478a49a5bdef19a0b3efc63cef9518e60f5e42366a0c24a2585ffb4'
