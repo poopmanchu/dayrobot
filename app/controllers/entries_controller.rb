465074d7913282ad67fc5b077cb0d4bd17905534
Ben Gross <brgross@gmail.com>
2011-05-23 10:48:46 -0400

    mail

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,7 +5,7 @@ class EntriesController < ApplicationController
 	
 	def create
 		message = Mail.new(params[:message])
-		@entry = Entry.new(:content => message.text_part)
+		@entry = Entry.new(:content => message.parts[0].body.decoded)
 		@entry.save
 		
 		#render :text => message.subject
